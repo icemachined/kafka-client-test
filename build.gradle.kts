d42b4b1f4af8af5b6ae2ee1c285d2c617329b921
Dmitry Morozovsky <27895587+icemachined@users.noreply.github.com>
2022-08-31 16:16:00 +0300

    test code + build and run configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.6.21"
+    kotlin("multiplatform") version "1.7.10"
+    id("com.ullink.nuget") version "2.23"
 }
 
 group = "org.example"
@@ -9,13 +10,14 @@ repositories {
     mavenCentral()
 }
 
+
 kotlin {
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
+        hostOs == "Mac OS X" -> macosX64()
+        hostOs == "Linux" -> linuxX64()
+        isMingwX64 -> mingwX64()
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
@@ -23,16 +25,30 @@ kotlin {
         binaries {
             executable {
                 entryPoint = "main"
+
+                if (isMingwX64) {
+                    linkTask.dependsOn("nugetRestore")
+                    val execPath = System.getenv("PATH") + System.getProperty("path.separator") + "$projectDir/packages/librdkafka.redist.1.9.2/runtimes/win-x64/native"
+                    runTask?.setEnvironment("PATH" to execPath)
+                }
             }
+
         }
     }
     sourceSets {
-        val nativeMain by getting {
+        val nativeMain by creating {
             dependencies {
                 implementation("io.github.icemachined:kafka-client:0.0.16")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1-native-mt")
             }
         }
-        val nativeTest by getting
+        nativeTarget.let {
+            getByName("${it.name}Main").dependsOn(nativeMain)
+        }
     }
+}
+
+tasks.withType<com.ullink.NuGetRestore>().configureEach {
+    packagesDirectory = rootProject.file("packages")
+    setPackagesConfigFile(rootProject.file("packages.config").relativeTo(project.projectDir).path)
 }
--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,86 @@
-fun main() {
-    println("Hello, Kotlin/Native!")
+import com.icemachined.kafka.clients.CommonConfigNames
+import com.icemachined.kafka.clients.consumer.*
+import com.icemachined.kafka.clients.producer.KafkaProducer
+import com.icemachined.kafka.clients.producer.ProducerRecord
+import com.icemachined.kafka.common.header.Header
+import com.icemachined.kafka.common.header.RecordHeader
+import com.icemachined.kafka.common.serialization.Serializer
+import com.icemachined.kafka.common.serialization.Deserializer
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
+fun main(args: Array<String>) {
+    val producerConfig = mapOf(
+        CommonConfigNames.BOOTSTRAP_SERVERS_CONFIG to "localhost:29092",
+        CommonConfigNames.CLIENT_ID_CONFIG to "test-consumer",
+        CommonConfigNames.LOG_LEVEL_NATIVE to "7"
+    )
+    val producer = KafkaProducer(producerConfig, object : Serializer<String> {
+        override fun serialize(data: String, topic: String?, headers: Iterable<Header>?): ByteArray? =
+            data.encodeToByteArray()
+    }, object : Serializer<String> {
+        override fun serialize(data: String, topic: String?, headers: Iterable<Header>?): ByteArray? =
+            data.encodeToByteArray()
+    })
+    runBlocking {
+        launch {
+            println("Start consume")
+            val consumerService = KafkaConsumerService(
+                ConsumerConfig(
+                    listOf("kkn-test"),
+                    mapOf(
+                        CommonConfigNames.BOOTSTRAP_SERVERS_CONFIG to "localhost:29092",
+                        CommonConfigNames.CLIENT_ID_CONFIG to "test-consumer",
+                        CommonConfigNames.GROUP_ID_CONFIG to "test-consumer-group",
+                        CommonConfigNames.LOG_LEVEL_NATIVE to "7",
+                        ConsumerConfigNames.ENABLE_AUTO_COMMIT_CONFIG to "false",
+                        ConsumerConfigNames.AUTO_OFFSET_RESET_CONFIG to "earliest"
+
+                    ),
+                    object : Deserializer<String> {
+                        override fun deserialize(data: ByteArray, topic: String?, headers: Iterable<Header>?): String {
+                            return data.decodeToString()
+                        }
+                    },
+                    object : Deserializer<String> {
+                        override fun deserialize(data: ByteArray, topic: String?, headers: Iterable<Header>?): String {
+                            return data.decodeToString()
+                        }
+                    },
+                    object : ConsumerRecordHandler<String, String> {
+                        override fun handle(record: ConsumerRecord<String, String>) {
+                            println("Key : ${record.key}, Value : ${record.value}, Headers: ${record.headers}")
+                        }
+                    }
+                )
+            )
+            consumerService.start()
+            println("Start delay")
+            yield()
+            delay(1000)
+            println("Sending messages")
+            val flow = producer.send(
+                ProducerRecord(
+                    "kkn-test", "new producer test", "test key",
+                    headers = listOf(RecordHeader("test.header.name", "test header value".encodeToByteArray()))
+                )
+            )
+            println("Got result ${flow.first()}")
+            val flow1 = producer.send(
+                ProducerRecord(
+                    "kkn-test", "new producer test 1", "test key",
+                    headers = listOf(RecordHeader("test.header.name1", "test header value1".encodeToByteArray()))
+                )
+            )
+            println("Got result ${flow1.first()}")
+            producer.close()
+            yield()
+            delay(10000)
+            println("End delay")
+            consumerService.stop()
+        }
+    }
 }
